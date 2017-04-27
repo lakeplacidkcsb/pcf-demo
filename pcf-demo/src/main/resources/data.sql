7a13187c1ca7e0e971148bd9986734a6d5028ca7
praneethramesh <sr.praneeth@gmail.com>
2017-04-27 19:27:01 -0400

    Adding Manifest file for CF deployment

--- pcf-demo/src/main/resources/data.sql
+++ pcf-demo/src/main/resources/data.sql
@@ -1,3 +1,6 @@
-INSERT INTO USER VALUES 
+DROP TABLE IF EXISTS user;
+CREATE TABLE user (id int NOT NULL, username varchar(255), PRIMARY KEY (id));
+
+INSERT INTO user VALUES 
 (1, 'root'),
 (2, 'admin');
